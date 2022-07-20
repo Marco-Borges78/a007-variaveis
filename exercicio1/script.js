0c0baa0ddb85bc11aa48a2b9a4604b71f2cecd11
Marco_Borges <mas.borges78@gmail.com>
2022-07-19 21:28:27 -0300

    Feito exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+const nome = prompt ("Qual o seu nome?")
+let idade = prompt ("Qual a sua idade?")
+console.log(typeof nome)
+console.log(typeof idade)
+
+// Na variavel idade o typeof ficou string pois a resposta não é um numeral.
+// Na variavel nome o typeof está correto.
+
+console.log("Olá",nome, "voçê tem",idade, "anos")
+
+//console.log (prompt("nome"))
+//console.log (prompt("idade"))
---
+++ exercicio3/script.js
@@ -0,0 +1,14 @@
+let a = 10
+let b = 25
+let c;
+
+//Aqui faremos a troca dos valores.
+
+c=b
+b=a
+a=c
+
+//Depopis da troca, teremos o seguinte resultado.
+
+console.log("O novo valor de a é" ,a)
+console.log("O novo valor de b é" ,b)
